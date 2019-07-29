ecbd4e44f5690131826fae7377cde0ff9f390860
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-07-29 09:45:52 -0500

    added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers;
+
+INSERT INTO burger_name (name)
+VALUES ("Mushroom Swiss Burger");
+
+INSERT INTO burger_name (name)
+VALUES ("Mac and Cheese Burger");
+
+INSERT INTO burger_name (name)
+VALUES ("PBJ Burger");
