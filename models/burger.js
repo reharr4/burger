25523cee767f32bf2d35ec8c09c45799ef4dd53c
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-07-31 20:34:09 -0500

    corrected orm method names

--- models/burger.js
+++ models/burger.js
@@ -3,24 +3,24 @@ var orm = require("../config/orm.js");
 
 var burger = {
     // select all burger table entries
-    all: function(cb){
-        orm.all("burger", function(res){
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
             cb(res);
         });
     },
     // variables cols and vals are arrays
-    create: function(cols, vals, cb){
-        orm.create("burger", cols, vals, function(res){
+    insertOne: function(cols, vals, cb){
+        orm.insertOne("burgers", cols, vals, function(res){
             cb(res);
         });
     },
     // objColVals is object specifying columns as object keys with associated values
-    update: function(objColVals, condition, cb){
-        orm.update("burger", objColVals, condition, function(res){
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
             cb(res);
         });
     },
 };
 
-// export database functions for controller
+// export database functions for controller (burgerController.js)
 module.exports = burger;
